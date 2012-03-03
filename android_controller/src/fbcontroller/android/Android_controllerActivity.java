c43a48ed6d5895d58fc77f81f17f0d82d3026a79
Saif Hasan <saifhhasan@gmail.com>
2012-03-03 14:52:41 +0530

    srs added

---
+++ android_controller/src/fbcontroller/android/Android_controllerActivity.java
@@ -0,0 +1,13 @@
+package fbcontroller.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Android_controllerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ client_controller/src/fbcontroller/client/Client_UI.java
@@ -0,0 +1,124 @@
+package fbcontroller.client;
+
+import org.eclipse.swt.graphics.*;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.StatusLineManager;
+import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.jface.window.ApplicationWindow;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Canvas;
+
+public class Client_UI extends ApplicationWindow {
+
+	/**
+	 * Create the application window.
+	 */
+	public Client_UI() {
+		super(null);
+		createActions();
+		addToolBar(SWT.FLAT | SWT.WRAP);
+		addMenuBar();
+		addStatusLine();
+	}
+
+	/**
+	 * Create contents of the application window.
+	 * @param parent
+	 */
+	@Override
+	protected Control createContents(Composite parent) {
+		Composite container = new Composite(parent, SWT.NONE);
+		
+		Canvas canvas = new Canvas(container, SWT.BORDER);
+		canvas.setBounds(101, 63, 357, 247);
+		
+		Image image = null;
+		
+		image = new Image(null, "/home/saif/Pictures/saif_hasan.jpg");
+		canvas.setSize(710, 924);
+		canvas.setBackgroundImage(image);
+
+		return container;
+	}
+
+	/**
+	 * Create the actions.
+	 */
+	private void createActions() {
+		// Create the actions
+	}
+
+	/**
+	 * Create the menu manager.
+	 * @return the menu manager
+	 */
+	@Override
+	protected MenuManager createMenuManager() {
+		MenuManager menuManager = new MenuManager("menu");
+		return menuManager;
+	}
+
+	/**
+	 * Create the toolbar manager.
+	 * @return the toolbar manager
+	 */
+	@Override
+	protected ToolBarManager createToolBarManager(int style) {
+		ToolBarManager toolBarManager = new ToolBarManager(style);
+		return toolBarManager;
+	}
+
+	/**
+	 * Create the status line manager.
+	 * @return the status line manager
+	 */
+	@Override
+	protected StatusLineManager createStatusLineManager() {
+		StatusLineManager statusLineManager = new StatusLineManager();
+		return statusLineManager;
+	}
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		try {
+			Client_UI window = new Client_UI();
+			window.setBlockOnOpen(true);
+			window.open();
+			Display.getCurrent().dispose();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Configure the shell.
+	 * @param newShell
+	 */
+	@Override
+	protected void configureShell(Shell newShell) {
+		super.configureShell(newShell);
+		newShell.setText("New Application");
+	}
+
+	/**
+	 * Return the initial size of the window.
+	 */
+	@Override
+	protected Point getInitialSize() {
+		return new Point(450, 300);
+	}
+}
